35c5e45467275d8e8af8620b57bfcc465704be9c
Акбархон Охунов <ohunovakbar565@gmail.com>
2025-09-07 03:14:36 +0500

    add new entities into ApplicationDbContext

--- QDryClean.Infrastructure/Persistence/ApplicationDbContext .cs
+++ QDryClean.Infrastructure/Persistence/ApplicationDbContext .cs
@@ -14,14 +14,44 @@ namespace QDryClean.Infrastructure.Persistance
         }
 
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Invoice> OrderInvoices { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
+        public DbSet<ItemCategory> ItemCategories { get; set; }
+        public DbSet<Charge> Charges { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasSequence<int>("ReceiptNumberSeq")
+                .StartsAt(1)
+                .IncrementsBy(1);
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ReceiptNumber)
+                .HasDefaultValueSql("NEXT VALUE FOR ReceiptNumberSeq");
+
+            modelBuilder.Entity<Order>()
+                .HasIndex(o => o.ReceiptNumber)
+                .IsUnique();
+
+
+            modelBuilder.Entity<Charge>()
+                .Property(c => c.Cost)
+                .HasPrecision(18, 0);
+
+            modelBuilder.Entity<Invoice>()
+                .Property(i => i.TotalCost)
+                .HasPrecision(18, 0);
+
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Points)
+                .HasPrecision(6, 2);
+
+            modelBuilder.Entity<Invoice>()
+                .Property(i => i.Discount)
+                .HasPrecision(6, 0); // если нужны проценты
+        }
     }
 }
 
-//protected override void OnModelCreating(ModelBuilder modelBuilder)
-//{
-//    modelBuilder.Entity<Marriage>()
-//        .HasOne(m => m.Groom)
-//        .WithOne(g => g.Marriage)
-//        .HasForeignKey<Marriage>(m => m.GroomId)
-//        .OnDelete(DeleteBehavior.NoAction);
-//}
--- QDryCleaning.Application/Absreactions/IApplicationDbContext.cs
+++ QDryCleaning.Application/Absreactions/IApplicationDbContext.cs
@@ -6,6 +6,12 @@ namespace QDryClean.Application.Absreactions
     public interface IApplicationDbContext
     {
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Invoice> OrderInvoices { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
+        public DbSet<ItemCategory> ItemCategories { get; set; }
+        public DbSet<Charge> Charges { get; set; }
 
         public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
