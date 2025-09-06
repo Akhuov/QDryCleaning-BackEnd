891d709b28e89ac4e28e707eb61933ef6052fc75
Акбархон Охунов <ohunovakbar565@gmail.com>
2025-09-07 00:42:29 +0500

    write order entity

--- QDryCleaning.Domain/Entities/Order.cs
+++ QDryCleaning.Domain/Entities/Order.cs
@@ -4,11 +4,14 @@ namespace QDryClean.Domain.Entities
 {
     public class Order : Auditable
     {
-        public int ReceiptNumber { get; set; }
-        public int CustomerId { get; set; }
-        //public Customer Customer { get; set; } 
-        //public OrderStatus Status { get; set; }
-        //public ICollection<Item> Items { get; set; } = new List<Item>();
-        //public OrderCharge OrderCharge { get; set; }
+        public required int ReceiptNumber { get; set; }
+        public required OrderStatus Status { get; set; }
+        public required string ProcessStatus { get; set; }
+        public required DateOnly ExpectedCompletionDate { get; set; }
+        public required int CustomerId { get; set; }
+        public required Customer Customer { get; set; }
+        public int? InvoiceId { get; set; }
+        public Invoice? Invoice { get; set; }
+        public string? Notes { get; set; }
     }
 }
