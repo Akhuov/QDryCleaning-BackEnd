9066df9d50647fb215083caac05f9b28eb4a6c64
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-07 16:34:00 +0500

    Set Token and fix it

---
+++ QDryClean.Api/Controllers/AuthsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QDryClean.Application.Absreactions;
+using QDryClean.Application.Dtos.AuthDTOs;
+using QDryClean.Application.Services.JWTServices;
+
+namespace QDryClean.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthsController : ControllerBase
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly ITokenService _tokenService;
+        private readonly IAuthService _authService;
+        public AuthsController(IApplicationDbContext context, ITokenService tokenService, IAuthService authService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+            _authService = authService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync([FromForm] AuthDTO dto)
+        {
+            try
+            {
+                var authService = new AuthService(_context, _tokenService);
+                var token = await _authService.LoginAsync(dto.LogIn, dto.Password);
+                return Ok(new { Token = token });
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ QDryCleaning.Application/Dtos/AuthDTOs/AuthDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace QDryClean.Application.Dtos.AuthDTOs
+{
+    public class AuthDTO
+    {
+        public string LogIn { get; set; }
+        public string Password { get; set; }
+    }
+}
