252116097df34ff7fa766f6275f8322fff269ba1
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-07 17:13:25 +0500

    Create User Commands and Handlers

---
+++ QDryCleaning.Application/UseCases/Users/Handlers/DeleteUserCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using QDryClean.Application.Absreactions;
+using QDryClean.Application.UseCases.Users.Commands;
+
+namespace QDryClean.Application.UseCases.Users.Handlers
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+
+        public DeleteUserCommandHandler(IApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var user = await _applicationDbContext.Users.FirstOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
+                if (user is not null)
+                {
+                    _applicationDbContext.Users.Remove(user);
+                    await _applicationDbContext.SaveChangesAsync(cancellationToken);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ QDryCleaning.Application/UseCases/Users/Handlers/GetByIdUserCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using QDryClean.Application.Absreactions;
+using QDryClean.Application.UseCases.Users.Quarries;
+using QDryClean.Domain.Entities;
+
+namespace QDryClean.Application.UseCases.Users.Handlers
+{
+    public class GetByIdUserCommandHandler : IRequestHandler<GetByIdUserCommand, User>
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+
+        public GetByIdUserCommandHandler(IApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<User> Handle(GetByIdUserCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var user = await _applicationDbContext.Users.FirstOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
+                if (user is not null)
+                {
+                    return user;
+                }
+                return new User();
+            }
+            catch (Exception)
+            {
+                return new User();
+            }
+        }
+    }
+}
