dda87c6329cacd752130455508a6d3b16100aa17
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-07 15:11:54 +0500

    Create Quarries to User

---
+++ QDryCleaning.Application/UseCases/Users/Quarries/GetAllUsersCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using QDryClean.Domain.Entities;
+
+namespace QDryClean.Application.UseCases.Users.Quarries
+{
+    public class GetAllUsersCommand : IRequest<List<User>>
+    {
+    }
+}
