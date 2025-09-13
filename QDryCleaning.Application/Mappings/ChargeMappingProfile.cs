e23fae5c3a8d227fffdce063f094ce7b18149d60
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-13 16:55:32 +0500

    Create ChargeMapp

---
+++ QDryCleaning.Application/Mappings/ChargeMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using QDryClean.Application.Dtos;
+using QDryClean.Application.UseCases.Charges.Commands;
+using QDryClean.Domain.Entities;
+
+namespace QDryClean.Application.Mappings
+{
+    public class ChargeMappingProfile : Profile
+    {
+        public ChargeMappingProfile()
+        {
+            CreateMap<Charge, ChargeDto>();
+            CreateMap<ChargeDto, CreateChargeCommand>().ReverseMap();
+            CreateMap<ChargeDto, Charge>()
+                .ForMember(dest => dest.ItemType, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedBy, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore());
+        }
+    }
+}
