c1080c46138f40596346794e14bff6fa08ab7386
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-15 17:49:33 +0500

    Charge CRUD Upgraded

--- QDryCleaning.Application/UseCases/charges/Handlers/CreateChargeCommandHandler.cs
+++ QDryCleaning.Application/UseCases/charges/Handlers/CreateChargeCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using QDryClean.Application.Absreactions;
 using QDryClean.Application.Common.Interfaces.Services;
 using QDryClean.Application.Dtos;
@@ -9,31 +10,40 @@ using QDryClean.Domain.Entities;
 
 namespace QDryClean.Application.UseCases.Charges.Handlers
 {
-    public class CreateChargeCommandHandler : IRequestHandler<CreateChargeCommand, ChargeDto>
+    public class CreateChargeCommandHandler : CommandHandlerBase, IRequestHandler<CreateChargeCommand, ChargeDto>
     {
-        private readonly IApplicationDbContext _applicationDbContext;
-        private readonly ICurrentUserService _currentUserService;
-        private IMapper _mapper;
-        public CreateChargeCommandHandler(IApplicationDbContext applicationDbContext, ICurrentUserService currentUserService, IMapper mapper)
-        {
-            _applicationDbContext = applicationDbContext;
-            _currentUserService = currentUserService;
-            _mapper = mapper;
-        }
+        public CreateChargeCommandHandler(
+            IApplicationDbContext applicationDbContext,
+            ICurrentUserService currentUserService,
+            IMapper mapper) : base(applicationDbContext, currentUserService, mapper) { }
         public async Task<ChargeDto> Handle(CreateChargeCommand request, CancellationToken cancellationToken)
         {
             try
             {
-                var charge = new Charge()
+
+                if (request.Name is null)
                 {
-                    Cost = request.Cost,
-                    Name = request.Name,
-                    CreatedAt = DateTime.UtcNow,
-                    CreatedBy = _currentUserService.UserId,
-                };
-                await _applicationDbContext.Charges.AddAsync(charge, cancellationToken);
-                await _applicationDbContext.SaveChangesAsync(cancellationToken);
-                return _mapper.Map<ChargeDto>(charge);
+                    throw new BadRequestExeption("Name field is required!");
+                }
+
+                var charge = await _applicationDbContext.Charges.FirstOrDefaultAsync(u => u.Name == request.Name, cancellationToken);
+                if (charge is null)
+                {
+                    charge = _mapper.Map<Charge>(request);
+                    charge.CreatedBy = _currentUserService.UserId;
+                    charge.CreatedAt = DateTime.Now;
+                    await _applicationDbContext.Charges.AddAsync(charge, cancellationToken);
+                    await _applicationDbContext.SaveChangesAsync(cancellationToken);
+                    return _mapper.Map<ChargeDto>(charge);
+                }
+                else
+                {
+                    throw new BadRequestExeption("Charge with this name already exists");
+                }
+            }
+            catch (BadRequestExeption)
+            {
+                throw;
             }
             catch (Exception ex)
             {
