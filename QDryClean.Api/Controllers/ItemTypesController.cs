30565bdf4df4058760654b1783593e0cc5c5c321
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-16 16:05:54 +0500

    Write Item Type Controller and fix somme errors in Charge

--- QDryClean.Api/Controllers/ItemTypesController.cs
+++ QDryClean.Api/Controllers/ItemTypesController.cs
@@ -2,57 +2,57 @@
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using QDryClean.Application.UseCases.Charges.Commands;
-using QDryClean.Application.UseCases.Charges.Quarries;
+using QDryClean.Application.UseCases.ItemTypes.Commands;
+using QDryClean.Application.UseCases.ItemTypes.Quarries;
 using QDryClean.Domain.Enums;
 
 namespace QDryClean.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ChargesController : ControllerBase
+    public class ItemTypesController : ControllerBase
     {
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
 
-        public ChargesController(IMediator mediator, IMapper mapper)
+        public ItemTypesController(IMediator mediator, IMapper mapper)
         {
             _mediator = mediator;
             _mapper = mapper;
         }
         [Authorize(Roles = $"{nameof(UserRole.Receptionist)},{nameof(UserRole.Admin)}")]
         [HttpPost]
-        public async Task<IActionResult> CreateChargeAsync(CreateChargeCommand command)
+        public async Task<IActionResult> CreateItemTypeAsync(CreateItemTypeCommand command)
         {
             var result = await _mediator.Send(command);
-            return Created("Charge created successfully.", result);
+            return Created("ItemType created successfully.", result);
         }
         [Authorize(Roles = $"{nameof(UserRole.Receptionist)},{nameof(UserRole.Admin)}")]
         [HttpDelete]
-        public async Task<IActionResult> DeleteChargeAsync(DeleteChargeCommand command)
+        public async Task<IActionResult> DeleteItemTypeAsync(DeleteItemTypeCommand command)
         {
             var result = await _mediator.Send(command);
             return Ok(result);
         }
         [Authorize(Roles = $"{nameof(UserRole.Receptionist)},{nameof(UserRole.Admin)}")]
         [HttpPut]
-        public async Task<IActionResult> UpdateChargeAsync(UpdateChargeCommand command)
+        public async Task<IActionResult> UpdateItemTypeAsync(UpdateItemTypeCommand command)
         {
             var result = await _mediator.Send(command);
             return Ok(result);
         }
         [HttpGet]
-        public async Task<IActionResult> GetAllChargesAsync()
+        public async Task<IActionResult> GetAllItemTypesAsync()
         {
-            var command = new GetAllChargesCommand();
+            var command = new GetAllItemTypesCommand();
             var result = await _mediator.Send(command);
             return Ok(result);
         }
 
         [HttpGet("{userId:int}")]
-        public async Task<IActionResult> GetByIdChargeAsync(int id)
+        public async Task<IActionResult> GetByIdItemTypeAsync(int id)
         {
-            var command = new GetByIdChargeCommand() { Id = id };
+            var command = new GetByIdItemTypeCommand() { Id = id };
             var result = await _mediator.Send(command);
             return Ok(result);
         }
