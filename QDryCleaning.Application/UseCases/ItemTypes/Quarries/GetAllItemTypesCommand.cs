910626f0fa9f08a9752d7b7bdba7ccd59a2dfa43
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-16 15:25:55 +0500

    Write Item Type Quarries

---
+++ QDryCleaning.Application/UseCases/ItemTypes/Quarries/GetAllItemTypesCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using QDryClean.Application.Dtos;
+
+namespace QDryClean.Application.UseCases.ItemTypes.Quarries
+{
+    public class GetAllItemTypesCommand : IRequest<List<ItemTypeDto>>
+    {
+    }
+}
---
+++ QDryCleaning.Application/UseCases/ItemTypes/Quarries/GetByIdItemTypeCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using QDryClean.Application.Dtos;
+
+namespace QDryClean.Application.UseCases.ItemTypes.Quarries
+{
+    public class GetByIdItemTypeCommand : IRequest<ItemTypeDto>
+    {
+        public int Id { get; set; }
+    }
+}
