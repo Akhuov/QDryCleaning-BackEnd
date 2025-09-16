db8f9fdb974c45c926a0a84f29cc778ce331a8a5
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-16 15:23:59 +0500

    ItemType Commands Created

---
+++ QDryCleaning.Application/UseCases/ItemTypes/Commands/UpdateItemTypeCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using QDryClean.Application.Dtos;
+
+namespace QDryClean.Application.UseCases.ItemTypes.Commands
+{
+    public class UpdateItemTypeCommand : ItemTypeDto,IRequest<ItemTypeDto>
+    {
+    }
+}
