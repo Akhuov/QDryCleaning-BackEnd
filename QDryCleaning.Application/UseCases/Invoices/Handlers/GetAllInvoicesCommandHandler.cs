fe4f198750e3648abd648bb40f67fe59988a4fed
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-17 23:08:40 +0500

    Write GetAll Invoices Command Handler

---
+++ QDryCleaning.Application/UseCases/Invoices/Handlers/GetAllInvoicesCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using QDryClean.Application.Absreactions;
+using QDryClean.Application.Common.Interfaces.Services;
+using QDryClean.Application.Dtos;
+using QDryClean.Application.Exceptions;
+using QDryClean.Application.UseCases.Invoices.Quarries;
+
+namespace QDryClean.Application.UseCases.Invoices.Handlers
+{
+    public class GetAllInvoicesCommandHandler : CommandHandlerBase, IRequestHandler<GetAllInvoicesCommand, List<InvoiceDto>>
+    {
+        public GetAllInvoicesCommandHandler(
+            IApplicationDbContext applicationDbContext,
+            ICurrentUserService currentUserService,
+            IMapper mapper) : base(applicationDbContext, currentUserService, mapper) { }
+
+        public async Task<List<InvoiceDto>> Handle(GetAllInvoicesCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var invoices = await _applicationDbContext.OrderInvoices.ToListAsync();
+
+                var list_of_invoiceDtos = new List<InvoiceDto>();
+                foreach (var invoice in invoices)
+                {
+                    list_of_invoiceDtos.Add(_mapper.Map<InvoiceDto>(invoice));
+                }
+
+                return list_of_invoiceDtos;
+
+            }
+            catch (Exception ex)
+            {
+                throw new InternalServerExeption(ex.Message);
+            }
+        }
+    }
+}
