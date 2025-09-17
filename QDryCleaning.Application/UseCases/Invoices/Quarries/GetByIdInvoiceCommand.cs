b8e48c1db9e074a9b033b7d4a470e451792b8266
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-17 22:06:21 +0500

    Create Invoice Quarries

---
+++ QDryCleaning.Application/UseCases/Invoices/Quarries/GetByIdInvoiceCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using QDryClean.Application.Dtos;
+
+namespace QDryClean.Application.UseCases.Invoices.Quarries
+{
+    public class GetByIdInvoiceCommand : IRequest<InvoiceDTO>
+    {
+        public Guid Id { get; set; }
+    }
+}
