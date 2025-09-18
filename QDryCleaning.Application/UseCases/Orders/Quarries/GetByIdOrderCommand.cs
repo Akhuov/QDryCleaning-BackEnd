1568d25f5dc9339f1ae84b34b5a933d59fcd11a9
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-18 22:20:27 +0500

    Write Quarries for Order

---
+++ QDryCleaning.Application/UseCases/Orders/Quarries/GetByIdOrderCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using QDryClean.Application.Dtos;
+
+namespace QDryClean.Application.UseCases.Orders.Quarries
+{
+    public class GetByIdOrderCommand : IRequest<OrderDto>
+    {
+    }
+}
