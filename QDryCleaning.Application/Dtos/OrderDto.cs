f9233163270423753279c728a223d6e3b2364995
akhun <azizkhon_akhunov@student.itpu.uz>
2025-09-18 22:15:02 +0500

    Write OrderDto

--- QDryCleaning.Application/Dtos/OrderDto.cs
+++ QDryCleaning.Application/Dtos/OrderDto.cs
@@ -1,9 +1,15 @@
 ﻿
+using QDryClean.Domain.Entities;
+using QDryClean.Domain.Enums;
+
 namespace QDryClean.Application.Dtos
 {
     public class OrderDto
     {
-        public int ReceiptNumber { get; set; }
+        public required int ReceiptNumber { get; set; }
+        public required ProcessStatus ProcessStatus { get; set; }
+        public required DateOnly ExpectedCompletionDate { get; set; }
         public int CustomerId { get; set; }
+        public string? Notes { get; set; }
     }
 }
